b90f05a03df5b3802a86913b33c2b5600bd529cf
Travis CI <travis@travis-ci.org>
2019-02-13 14:47:11 -0500

    added goodies

--- src/main/java/frc/team5115/robot/Main.java
+++ src/main/java/frc/team5115/robot/Main.java
@@ -5,9 +5,10 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.team5115.robot.robot;
+package frc.team5115.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.team5115.robot.robot.Robot;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all.
